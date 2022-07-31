7464335f928335060148e7ea74f277feae597f7e
Krishanadave671 <84977709+Krishanadave671@users.noreply.github.com>
2022-07-31 11:30:40 +0530

    code saved

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config(); 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DB = process.env.MONGO_URI; 
 // middlewares
 
@@ -15,6 +15,8 @@ mongoose.connect(DB).then( () => {
 app.get('/', (req, res) => {
     res.send("Hello World"); 
 }); 
+
+// listen to port
 app.listen(PORT , "0.0.0.0" , ()=> {
     console.log(`Server is running on port ${PORT}`); 
 })
---
+++ server/models/vendingzones.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const vendingzoneSchema = new mongoose.Schema({
+    
+
+}) 
+
+const vendingzones = mongoose.model('vendingzone', vendingzoneSchema);
+module.exports = vendingzones; 
