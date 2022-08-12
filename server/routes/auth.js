114738f5dd7eb74361baca8ce34539e11802b3c0
Yashna Dawda <76972236+xanthena@users.noreply.github.com>
2022-08-13 02:13:03 +0530

    login and signup apis

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,8 +5,60 @@ const authRouter = express.Router();
 const jwt = require("jsonwebtoken");
 
 //Sign Up
-// authRouter.post("/api/signup", async (req, res) => {
-//     try {
-//         const { name, address, }
-//     }
-// });
+authRouter.post("/api/signup", async (req, res) => {
+    try {
+        const { name, address, dob, gender, phone, aadharno, pancardno, 
+                passport, electionid, mcgmlicense, aadharcard, pancard, shoplocation} = req.body;
+        
+        const existingVendor = await Vendor.findOne({ phone });
+        if (existingVendor) {
+            return res
+            .status(400)
+            .json({ msg: "Vendor with same phone number exists! "});
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 8);
+
+        let vendor = new Vendor({
+            name,
+            dob,
+            gender,
+            address,
+            password: hashedPassword,
+            phone,
+            aadharno,
+            pancardno,
+            passport,
+            electionid,
+            mcgmlicense,
+            aadharcard,
+            pancard,
+            shoplocation
+        });
+        vendor = await vendor.save();
+        res.json(vendor);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+//Sign In
+authRouter.post("/api/login", async (req, res) =>{
+    try {
+        const { phone, password } = req.body;
+    
+        const vendor = await Vendor.findOne({ phone });
+        if (!vendor) {
+          return res
+            .status(400)
+            .json({ msg: "User with this phone number does not exist!" });
+        }
+    
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if (!isMatch) {
+          return res.status(400).json({ msg: "Incorrect password." });
+        }
+    }catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
