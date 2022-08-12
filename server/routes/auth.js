ead572caa30c0a800a6581952c4ce9d5c5345dfa
Yashna Dawda <76972236+xanthena@users.noreply.github.com>
2022-08-12 17:01:56 +0530

    vendor schema

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,3 +1,12 @@
 const express = require('express'); 
-const authrouter = express.authrouter ; 
+const Vendor = require("../models/vendor_details");
+const bcryptjs = require("bcryptjs");
+const authRouter = express.Router();
+const jwt = require("jsonwebtoken");
 
+//Sign Up
+authRouter.post("/api/signup", async (req, res) => {
+    try {
+        const { name, address, }
+    }
+})
