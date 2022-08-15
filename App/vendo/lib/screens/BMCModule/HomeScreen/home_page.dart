b520c4bde65e307699d6ed24414b1932e272767c
ypdalvi <ypdalvi@gmail.com>
2022-08-15 18:48:41 +0530

    bmc module

---
+++ App/vendo/lib/screens/BMCModule/HomeScreen/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BMCHomePage extends StatefulWidget {
+  const BMCHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<BMCHomePage> createState() => _BMCHomePageState();
+}
+
+class _BMCHomePageState extends State<BMCHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
