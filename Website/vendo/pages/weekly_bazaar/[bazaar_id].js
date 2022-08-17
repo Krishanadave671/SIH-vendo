46000e68d11c02d31b4dea4bf02beb74c3e1a2e6
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2022-08-18 01:00:02 +0530

    Added weekly bazaar

--- Website/vendo/pages/weekly_bazaar/[bazaar_id].js
+++ Website/vendo/pages/weekly_bazaar/[bazaar_id].js
@@ -1,10 +1,82 @@
+import Highlighter from "../components/Highlighter";
 import Navbar from "../components/Navbar2";
+import React from "react";
+import {
+  GoogleMap,
+  LoadScript,
+  MarkerF,
+  Autocomplete,
+} from "@react-google-maps/api";
 
 export default function Bazaar({BazaarID}){
 
+  const containerStyle = {
+    // width: '800px',
+    height: "400px",
+    marginTop: "20px",
+  };
+  
+  const [center, setCenter] = React.useState({
+    lat: 19.076,
+    lng: 72.8777,
+  });
+  
+  const [position, setPosition] = React.useState({
+    lat: 37.772,
+    lng: -122.214,
+  });
+
     return(
-        <div>
-            Bazaar1
+        <div className="bazaar-main-container">
+          <Highlighter Text="Bazaar ID - B1234234" fontSize="1.6rem"/>
+          <div className="bazaar-flex">
+            <div>
+              <Highlighter Text="Date of the event" fontSize="1.2rem"/>
+              <div className="pending-application-section-desc">16/04/2003</div>
+              <Highlighter Text="Maximum vendors capacity" fontSize="1.2rem"/>
+              <div className="pending-application-section-desc">12</div>
+              <Highlighter Text="Bazaar Description" fontSize="1.2rem"/>
+              <div className="pending-application-section-desc">Best bazaar</div>
+            </div>
+            <div>
+              <Highlighter Text="Location of Event" fontSize="1.2rem"/>
+            <LoadScript
+                  googleMapsApiKey="AIzaSyClwDKfzGV_7ICoib-lk2rH0iw5IlKW5Lw"
+                  libraries={["places"]}
+                >
+                  <GoogleMap
+                    id="marker-example"
+                    mapContainerStyle={containerStyle}
+                    center={center}
+                    zoom={16}
+                    onClick={(e) => {
+                      setPosition({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+                      Geocode.fromLatLng(
+                        position.lat.toString(),
+                        position.lng.toString()
+                      ).then(
+                        (response) => {
+                          console.log(response.results[0].formatted_address);
+                          setAddress(response.results[0].formatted_address);
+                        },
+                        (error) => {
+                          console.log(error);
+                        }
+                      );
+                    }}
+                  >
+                    <MarkerF
+                      // onLoad={onLoad}
+                      position={position}
+                    />
+                    {/* Child components, such as markers, info windows, etc. */}
+                    {/* <></> */}
+                    <MarkerF />
+                  </GoogleMap>
+                  </LoadScript>
+            </div>
+          </div>
+          
         </div>
     );
 }
