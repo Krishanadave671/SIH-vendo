2c8477a5caf43a75d5b8dde9fa745b6d773eacb8
Yashna Dawda <76972236+xanthena@users.noreply.github.com>
2022-08-19 01:13:49 +0530

    vendorschema updated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,8 +8,12 @@ const auth = require("../middlewares/auth");
 //Sign Up
 authRouter.post("/api/signup", async (req, res) => {
     try {
-        const {vendorid , vendorcategory , name, address, dob, gender, phone, aadharno, pancardno,password ,  
-                passport, electionid, mcgmlicense, aadharcard, pancard, shoplocation, isapproved} = req.body;
+        const {vendorId , vendorCategory , name, address, dob, gender, phone, aadharNo, panCardNo,password ,  
+          isPassport, isElectionid, isMcgmLicense, aadharcardImageUrl, pancardImageUrl, shopLocationAddress, 
+          shopLocationLat,
+          shopLocationLong,
+          vendingZoneIdApplied,
+          shopCity, isApproved} = req.body;
         
         const existingVendor = await Vendor.findOne({ phone });
         if (existingVendor) {
@@ -21,23 +25,27 @@ authRouter.post("/api/signup", async (req, res) => {
         const hashedPassword = await bcryptjs.hash(password, 8);
        
         let vendor = new Vendor({
-          vendorid , 
+          vendorId , 
             name,
             dob,
             gender,
             address,
             password: hashedPassword,
             phone,
-            aadharno,
-            pancardno,
-            passport,
-            electionid,
-            mcgmlicense,
-            aadharcard,
-            pancard,
-            shoplocation, 
-            vendorcategory, 
-            isapproved
+            aadharNo,
+            panCardNo,
+            isPassport,
+            isElectionid,
+            isMcgmLicense,
+            aadharcardImageUrl,
+            pancardImageUrl,
+            shopLocationAddress, 
+            shopLocationLat,
+            shopLocationLong,
+            vendingZoneIdApplied,
+            shopCity,
+            vendorCategory, 
+            isApproved
         });
         vendor = await vendor.save();
         res.json(vendor);
@@ -70,7 +78,7 @@ authRouter.post("/api/login", async (req, res) =>{
     }
 });
 
-authRouter.post("/tokenIsValid", async (req, res) => {
+authRouter.post("/api/tokenIsValid", async (req, res) => {
     try {
       const token = req.header("x-auth-token");
       if (!token) return res.json(false);
@@ -86,9 +94,9 @@ authRouter.post("/tokenIsValid", async (req, res) => {
   });
 
   // get user data
-  // authRouter.get("/", auth, async (req, res) => {
-  //   const vendor = await Vendor.findById(req.vendor);
-  //   res.json({ ...vendor._doc, token: req.token });
-  // });
+  authRouter.get("/api/getuserdata", auth, async (req, res) => {
+    const vendor = await Vendor.findById(req.vendor);
+    res.json({ ...vendor._doc, token: req.token });
+  });
 
   module.exports = authRouter;
