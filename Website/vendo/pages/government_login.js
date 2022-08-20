7fc213bc46f55fdca94e2af544efb2ac4881feec
Ananya Garg <gargananya2@gmail.com>
2022-08-20 21:06:18 +0530

    updated website

--- Website/vendo/pages/government_login.js
+++ Website/vendo/pages/government_login.js
@@ -1,37 +1,50 @@
-import React , { useEffect }from 'react'
-import Button from 'react-bootstrap/Button';
-import Router from 'next/router';
-import CommonInput from './components/CommonInput';
-import styles from '../styles/Home.module.css';
+import React, { useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Router from "next/router";
+import CommonInput from "./components/CommonInput";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import loginimage from "../Images/LoginScreen1.png";
 
 export default function government_login() {
-  var govtID = "", password = "";
-  var login= e =>{
+  var govtID = "",
+    password = "";
+  var login = (e) => {
     // console.log(govtID);
     // console.log(password)
-    Router.push('/dashboard')
-  }
+    Router.push("/dashboard");
+  };
 
   return (
-    <div className='govt-login-container'>
-      <div style={{height:"40px"}}/>
-      <div className='ContainerForLogin'>
-      <div className='VendoTitle'>Vendo</div>
-      <div className='VendoTitle2'>Government Login</div>
-      <div className='InputForLogin'>
-      <CommonInput placeholderText='Enter Govt official ID' onChange={(e)=>{
-        govtID = e.target.value;
-      }}/>
-      <CommonInput placeholderText='Enter password'onChange={(e)=>{
-        password = e.target.value;
-      }}/>
+    <div className="govt-login-container">
+      <div className="ContainerForLogin">
+        <div className="closeGridVerticalHeaderContainer">
+        <h1 className="vendoHeaderLoginScreen">Vendo</h1>
+        <h1 className="vendoTextLoginScreen">Government Login</h1>
+        </div>
+        <div className="InputForLogin">
+          <CommonInput
+            placeholderText="Enter Govt official ID"
+            onChange={(e) => {
+              govtID = e.target.value;
+            }}
+          />
+          <CommonInput
+            placeholderText="Enter password"
+            onChange={(e) => {
+              password = e.target.value;
+            }}
+          />
+        </div>
+        <div className="containerforbutton">
+          <Button className="button" onClick={login}>
+            Login
+          </Button>
+        </div>
       </div>
-      <div className='containerforbutton'>
-      <Button  className='button' onClick={login}>Login</Button>
+      <div classname="ImageForLogin">
+        <Image src={loginimage} ></Image>
       </div>
-      </div>
-      
     </div>
-  )
-
+  );
 }
