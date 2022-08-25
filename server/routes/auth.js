abb929467fe8ff9103cd3ae8d276c80b5a0c0029
Yashna Dawda <76972236+xanthena@users.noreply.github.com>
2022-08-26 01:26:36 +0530

    in out time done

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -176,4 +176,20 @@ authRouter.get("/api/getvendors/approved/:location", async(req, res) => {
     }
 })
 
+authRouter.post("/api/setvendortime", async(req, res) => {
+    try{
+        const {vendorId, inOrOut, time} = req.body;
+        let vendor = await Vendor.findOne({vendorId});
+        if (inOrOut == true){
+            await Vendor.updateOne({vendorId},  {inOrOut: true, "$push": {inTime: time}});
+        }
+        else {
+            await Vendor.updateOne({vendorId}, {inOrOut: false, "$push": {outTime: time}});
+        }
+        res.status(200).json(vendor);
+    }catch (e) {
+        res.status(500).json({ e: e.message });
+    }
+})
+
 module.exports = authRouter;
