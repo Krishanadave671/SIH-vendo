96cb9d361525511d8e998888ae786f853bab8cf4
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2022-08-26 02:48:01 +0530

    Improved UI and backend integration

--- Website/vendo/pages/complaints_section/[complaint_id].js
+++ Website/vendo/pages/complaints_section/[complaint_id].js
@@ -20,6 +20,10 @@ function VendorApplicationDetails({ ComplaintDetails, VendorDetails }) {
     marginTop: "20px",
   };
 
+  // console.log(ComplaintDetails);
+  // console.log(VendorDetails);
+  // console.log("http://localhost:4000/api/getvendorsfromId/" + ComplaintDetails.vendorId);
+
   const [center, setCenter] = React.useState({
     lat: VendorDetails.shopLocationLat % 90,
     lng: VendorDetails.shopLocationLong % 90,
@@ -35,6 +39,13 @@ function VendorApplicationDetails({ ComplaintDetails, VendorDetails }) {
   const MainContainer = () => {
     return (
       <div className="approved-application-main-container">
+        <div className="pending-application-section-title">Vendor ID </div>
+        <div className="pending-application-section-desc">
+          <a href={"/approved_application/" + VendorDetails.vendorId }>
+          {VendorDetails.vendorId}
+          </a>
+            
+        </div>
         <div className="pending-application-section-title">Vendor Name </div>
         <div className="pending-application-section-desc">
           {VendorDetails.name}
@@ -42,14 +53,14 @@ function VendorApplicationDetails({ ComplaintDetails, VendorDetails }) {
         <div className="pending-application-section-title">Documents </div>
         <div className="pending-application-docs">
           <a
-            href="https://firebasestorage.googleapis.com/v0/b/dhatnoon-backend.appspot.com/o/DBMS_Notes.pdf?alt=media&token=6a001bc9-3a6c-4cd0-8324-460caeab3f15"
+            href={VendorDetails.aadharcardImageUrl}
             target="_blank"
             rel="noreferrer"
           >
             View Aadhar Card
           </a>
           <a
-            href="https://firebasestorage.googleapis.com/v0/b/dhatnoon-backend.appspot.com/o/DBMS_Notes.pdf?alt=media&token=6a001bc9-3a6c-4cd0-8324-460caeab3f15"
+            href={VendorDetails.pancardImageUrl}
             target="_blank"
             rel="noreferrer"
           >
@@ -68,6 +79,8 @@ function VendorApplicationDetails({ ComplaintDetails, VendorDetails }) {
         <div className="pending-application-section-desc">
           {ComplaintDetails.complaintDescription}
         </div>
+        <div className="pending-application-section-title">Image</div>
+        <img src={ComplaintDetails.complaintImageUrl} alt="" />
         <div className="pending-application-section-title">
           Vendor Location{" "}
         </div>
@@ -111,9 +124,11 @@ export async function getServerSideProps(context) {
   );
   const data = await JSON.parse(JSON.stringify(res.data));
   const res2 = await axios.get(
-    "http://localhost:4000/api/getvendorsfromID/VX003"
+    "http://localhost:4000/api/getvendorsfromId/" + data[0].vendorId
   );
   const data2 = await JSON.parse(JSON.stringify(res2.data));
+  // console.log(data2);
+  console.log("dbfvjhdbvhbetsj");
   return {
     props: {
       ComplaintDetails: data[0],
